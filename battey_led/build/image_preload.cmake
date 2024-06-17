cba8f367bcfb78e5ea0cf815692e5285e1c869f1
YangByeongHun <home@YangByeongHuns-MacBook-Pro.local>
2024-06-17 21:29:18 +0900

    battery

---
+++ battey_led/build/image_preload.cmake
@@ -0,0 +1,8 @@
+# Generated file that can be used to preload variant images
+set(BOARD "nrf52840dk_nrf52840" CACHE INTERNAL "NCS child image controlled")
+set(BOARD_ROOT "/Users/home/Downloads/LEDMatrix_1;/Users/home/Desktop/ncs/v2.5.0/zephyr/samples/drivers/led_ws2812;/Users/home/Downloads/led_ws2812_original;/Users/home/Downloads/timer;/Users/home/Downloads/encoder;/Users/home/Downloads/intergrate_led_encdoder;/Users/home/Downloads/rgb_led;/Users/home/Desktop/ncs/v2.5.0/nrf;/Users/home/Desktop/ncs/v2.5.0/zephyr" CACHE INTERNAL "NCS child image controlled")
+set(CACHED_CONF_FILE "/Users/home/Downloads/rgb_led/prj.conf" CACHE INTERNAL "NCS child image controlled")
+set(CONF_FILE_BUILD_TYPE "" CACHE INTERNAL "NCS child image controlled")
+set(DTC_OVERLAY_FILE "/Users/home/Downloads/rgb_led/boards/nrf52840dk_nrf52840.overlay" CACHE INTERNAL "NCS child image controlled")
+set(NCS_TOOLCHAIN_VERSION "NONE" CACHE INTERNAL "NCS child image controlled")
+set(WEST_PYTHON "/opt/nordic/ncs/toolchains/20d68df7e5/opt/python@3.9/bin/python3.9" CACHE INTERNAL "NCS child image controlled")
---
+++ battey_led/src/main.c
@@ -0,0 +1,92 @@
+#include <zephyr/kernel.h>
+#include <zephyr/device.h>
+#include <zephyr/devicetree.h>
+#include <zephyr/sys/util.h>
+
+#include "led.h"
+#include "batterydisplay.h"
+#include "value.h"
+
+struct k_timer my_timer;
+struct k_work my_work;
+
+static int seconds = 0;
+
+void my_work_handler(struct k_work *work)
+{
+        printk("Time: %d\n", seconds);
+
+        // Display the time on the LED matrix
+        led_on_seconds(seconds);
+
+        // // Display the time on the battery display
+        // if( seconds == 0){
+        //         display_level(0);
+        // } else if(seconds > 0 && seconds < 10){
+        //         display_level(1); // 빨간
+        // } else if(seconds >= 10 && seconds < 20){
+        //         display_level(2);
+        // } else if(seconds >= 20 && seconds < 30){
+        //         display_level(3); 
+        // } else if(seconds >= 30 && seconds < 40){
+        //         display_level(4); // 주황
+        // } else if(seconds >= 40 && seconds < 50){
+        //         display_level(5); 
+        // } else if(seconds >= 50 && seconds < 56){
+        //         display_level(6); // 초록
+        // } else {
+        //         display_level(7); // 파랑
+        // }
+
+        // Display the time on the battery display
+        if( seconds == 0){
+                display_level(0);
+        } else if(seconds > 0 && seconds <= 15){
+                display_level(1); // 빨간
+        } else if(seconds > 15 && seconds <= 30){
+                display_level(4); // 주황
+        } else if(seconds > 30 && seconds <= 45){
+                display_level(6); // 초록
+        } else {
+                display_level(7); // 파랑
+        }        
+
+
+        // Increment the seconds
+        seconds = seconds + 15;
+        if(seconds > 60){
+                seconds = 0;
+        }
+}
+
+K_WORK_DEFINE(my_work, my_work_handler);
+
+void my_timer_handler(struct k_timer *dummy)
+{
+        k_work_submit(&my_work);
+}
+
+K_TIMER_DEFINE(my_timer, my_timer_handler, NULL);
+
+int main(void)
+{
+        printk("Seconds counter example 2\n");
+        int ret = DK_OK;
+
+        ret = batterydisplay_intit();
+        if(ret != DK_OK){
+                printk("Error initializing battery display\n");
+                return DK_ERR;
+        }
+
+        ret = led_init();
+        if(ret != DK_OK){
+                printk("Error initializing LED\n");
+                return DK_ERR;
+        }
+
+        // Start the timer
+        k_timer_start(&my_timer, K_SECONDS(1), K_SECONDS(1));
+
+        return DK_OK;
+}
