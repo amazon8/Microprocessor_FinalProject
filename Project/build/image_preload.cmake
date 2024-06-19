3c5da65b976f410887740f074e2ded9b1b6ff604
gunminy <gunmin0525@gmail.com>
2024-06-19 13:52:13 +0900

    add bpm checking

---
+++ Project/build/image_preload.cmake
@@ -0,0 +1,8 @@
+# Generated file that can be used to preload variant images
+set(BOARD "nrf52840dk_nrf52840" CACHE INTERNAL "NCS child image controlled")
+set(BOARD_ROOT "c:/ncs/myapps/led_ws2812;c:/ncs/myapps/project;C:/ncs/v2.5.0/nrf;C:/ncs/v2.5.0/zephyr" CACHE INTERNAL "NCS child image controlled")
+set(CACHED_CONF_FILE "c:/ncs/myapps/Project/prj.conf" CACHE INTERNAL "NCS child image controlled")
+set(CONF_FILE_BUILD_TYPE "" CACHE INTERNAL "NCS child image controlled")
+set(DTC_OVERLAY_FILE "c:/ncs/myapps/Project/boards/nrf52840dk_nrf52840.overlay" CACHE INTERNAL "NCS child image controlled")
+set(NCS_TOOLCHAIN_VERSION "NONE" CACHE INTERNAL "NCS child image controlled")
+set(WEST_PYTHON "C:/ncs/toolchains/cf2149caf2/opt/bin/python.exe" CACHE INTERNAL "NCS child image controlled")
